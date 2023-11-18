0a379e287068251baa3661dc71b3d6556f4da2a6
Mghiii <abd.mghimimi@gmail.com>
2023-11-18 21:34:21 +0100

    commit

---
+++ style/src/App.js
@@ -0,0 +1,11 @@
+import Tailwind from "./components/Tailwind";
+
+function App() {
+  return (
+    <div className="App">
+      <Tailwind />
+    </div>
+  );
+}
+
+export default App;
---
+++ style/src/components/Tailwind.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Tailwind() {
+  return (
+    <div >
+      <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md mb-4">
+        <div className="p-4">
+          <p className="text-lg font-semibold">Card 1</p>
+          <p>dfs</p>
+        </div>
+      </div>
+      <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-lg mb-4">
+        <div className="p-4">
+          <p className="text-lg font-semibold">Card 2</p>
+          <p>fsd</p>
+        </div>
+      </div>
+      <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-xl mb-4">
+        <div className="p-4">
+          <p className="text-lg font-semibold">Card 3</p>
+          <p>fds</p>
+        </div>
+      </div>
+      <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-2xl mb-4">
+        <div className="p-4">
+          <p className="text-lg font-semibold">Card 4</p>
+          <p>fsd</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tailwind;
