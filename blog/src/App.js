33a84be84134dee055827fe0ea7999737fd26eb8
Mghiii <abd.mghimimi@gmail.com>
2023-12-10 06:20:50 +0100

    commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,26 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import SingleProduct from "./components/pages/SingleProduct";
-import Navbar from "./components/Navbar";
-import products from "./data";
+import Blog from "./components/Blog";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home products={products} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/product/:id"
-          element={<SingleProduct products={products} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Blog />
+    </div>
   );
 }
 
---
+++ blog/src/components/Blog.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Blog() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="bg-white rounded-lg overflow-hidden shadow-md"
+        >
+          <img
+            src={product.image}
+            alt=""
+            className="w-full h-40 object-cover"
+          />
+          <div className="p-4">
+            <h3 className="text-lg font-semibold mb-2">{product.title}</h3>
+            <p>ID: {product.id}</p>
+            <p>Price: ${product.price}</p>
+            <p className="mt-2">{product.description}</p>
+            <div className="mt-3">
+              <p className="font-semibold">Rating:</p>
+              <ul className="list-none pl-0">
+                <li>Rate: {product.rating.rate}</li>
+                <li>Count: {product.rating.count}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
